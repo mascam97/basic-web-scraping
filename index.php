c0409b1f0af5ca2c37bf2a79e5a7706eedb67990
Martín Campos <martin.stepwolf@gmail.com>
2021-03-31 13:48:05 -0600

    margin, padding, width and text style

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 </head>
 
 <body class="bg-gray-100">
-        <header class="bg-gray-200">
-                <h2>Basic Web Scraping</h2>
+        <header class="p-4 bg-gray-200">
+                <h2 class="capitalize text-2xl font-bold text-center">Basic Web Scraping</h2>
         </header>
-        <main>
-                <div class="border-2 border-gray-400 text-gray-900">
+        <main class="m-2">
+                <div class="p-2 border-2 border-gray-400 text-gray-900">
                         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get">
                                 <div>
                                         <label for="url">Type a URL (preferably in spanish):</label>
@@ -24,29 +24,29 @@
                                 </div>
                                 <button type="submit">Get information</button>
                         </form>
-                        <div>
+                        <p class="text-sm">
                                 Description: Project to get source code (HTML) from a website and process it to clean information (no html tags, javascript, css code and some words) and get the most common words.
                                 <a href="https://github.com/martin-stepwolf/basic-web-scraping" target="_blank" rel="noopener noreferrer">Source code</a> <br>
                                 <strong>Note: This website does not collect information about you and your interaction (neither cookies nor database).</strong>
-                        </div>
+                        </p>
                 </div>
-                <div>
-                        <div class="border-2 border-gray-400 bg-blue-200 text-blue-800">
-                                <h4>Most common words</h4>
-                                <ol id="most-common-words"></ol>
+                <div class="mt-2">
+                        <div class="p-2 border-2 w-1/2 inline-block border-gray-400 bg-blue-200 text-blue-800">
+                                <h4 class="text-lg font-semibold">Most common words</h4>
+                                <ol class="capitalize" id="most-common-words"></ol>
                         </div>
-                        <div class="border-2 border-gray-400  bg-red-200 text-red-800">
-                                <h4>Most common tags</h4>
+                        <div class="p-2 mt-2 w-1/2 inline-block border-2 border-gray-400  bg-red-200 text-red-800">
+                                <h4 class="text-lg font-semibold">Most common tags</h4>
                                 <ol id="most-common-tags"></ol>
                         </div>
-                        <div class="border-2 border-gray-400 bg-green-200 text-green-800">
-                                <h4>Information</h4>
+                        <div class="p-2 mt-2 border-2 border-gray-400 bg-green-200 text-green-800">
+                                <h4 class="text-lg font-semibold">Information</h4>
                                 <hr>
-                                <p id="info"></p>
+                                <p class="tracking-wide leading-relaxed text-justify" id="info"></p>
                         </div>
-                        <div class="border-2 border-gray-400 bg-gray-200 text-gray-800">
-                                <h3>Data</h3>
-                                <p id="text">
+                        <div class="p-2 mt-2 border-2 border-gray-400 bg-gray-200 text-gray-800">
+                                <h3 class="text-lg font-semibold">Data</h3>
+                                <p class="tracking-wide leading-relaxed text-justify" id="text">
                                         <?php
                                         if (isset($_GET['url'])) {
                                                 $lines = file($_GET['url']);
